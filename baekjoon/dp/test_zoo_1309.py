d148c25435efe6b0c8ffc314296c92687906691c
sang-il <lsi9393@naver.com>
2024-03-22 17:04:48 +0900

    add modular calculation

--- baekjoon/dp/test_zoo_1309.py
+++ baekjoon/dp/test_zoo_1309.py
@@ -4,15 +4,16 @@ https://www.acmicpc.net/problem/1309
 4
 '''
 N = int(input())
-
+mod = 9901
 
 def solution(N):
     lion = 2
     empty = 1
     for i in range(2, N + 1):
-        lion, empty = lion + 2 * empty, lion + empty
+        # lion, empty = lion + 2 * empty, lion + empty
+        lion, empty = (lion + 2 * empty) % mod, (lion + empty) % mod
 
     return lion + empty
 
 
-print(solution(N) % 9901)
+print(solution(N) % mod)
